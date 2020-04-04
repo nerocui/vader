21d4a868e553cdd87d22e846d4cf88c428f24769
nerocui <nerocui@outlook.com>
2020-04-04 02:11:02 -0700

    added test for evaluations

---
+++ Vader.Tests/CodeAnalysis/EvaluationTests.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Vader.CodeAnalysis;
+using Vader.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Vader.Tests.CodeAnalysis
+{
+    public class EvaluationTests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("+1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("11 + 33", 44)]
+        [InlineData("65 - 21", 44)]
+        [InlineData("11 * 12", 132)]
+        [InlineData("32 / 4", 8)]
+        [InlineData("(22)", 22)]
+        [InlineData("(1 + 2) * 2", 6)]
+        [InlineData("(1 * 2) /2", 1)]
+        [InlineData("1 == 2", false)]
+        [InlineData("33 != 22", true)]
+        [InlineData("true && false", false)]
+        [InlineData("true || false", true)]
+        [InlineData("true && 2 == 3", false)]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        [InlineData("!true", false)]
+        [InlineData("!false", true)]
+        [InlineData("(a = 10) * 8", 80)]
+        public void SyntaxFact_GetText_RoundTrips(string text, object expectedValue)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+            var compilation = new Compilation(syntaxTree);
+            var variables = new Dictionary<VariableSymbol, object>();
+            var result = compilation.Evaluate(variables);
+
+            Assert.Empty(result.Diagnostics);
+            Assert.Equal(expectedValue, result.Value);
+        }
+    }
+}
